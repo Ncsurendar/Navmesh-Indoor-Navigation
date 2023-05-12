7b5e281a53a2a56d005a85c21c8d7b5bc1148ea8
Ncsurendar <ncsurendar123@gmail.com>
2023-05-12 14:25:41 +0530

    Initial Commit

---
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera cam;
+
+    public UnityEngine.AI.NavMeshAgent agent;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                // MOVE THE AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+		
+	}
+}
